77ab3f4558d37af4823c97eaf3ba20b1bdb9bcb7
Abby Bryant <rand0m-cloud@outlook.com>
2024-12-04 19:42:46 -0500

    wip

--- bam2gltf/app/src/main/kotlin/org/toontownkt/bam/gltf/Main.kt
+++ bam2gltf/app/src/main/kotlin/org/toontownkt/bam/gltf/Main.kt
@@ -1,11 +1,11 @@
-@file:OptIn(ExperimentalUnsignedTypes::class, ExperimentalSerializationApi::class)
+@file:OptIn(ExperimentalUnsignedTypes::class, ExperimentalSerializationApi::class, ExperimentalEncodingApi::class)
 
 package org.toontownkt.bam.gltf
 
-import gltf.GlTF
+import gltf.*
 import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.decodeFromStream
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.*
 import kotlinx.serialization.json.internal.readJson
 import org.toontownkt.bam.BamFile
 import org.toontownkt.bam.RawBamFile
@@ -13,6 +13,10 @@ import org.toontownkt.bam.types.*
 import java.io.File
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
+
+private val json = Json { prettyPrint = true }
 
 fun main() {
     val bamFile = File("../../library/open-toontown-resources/phase_3/models/char/mickey-1200.bam")
@@ -20,6 +24,7 @@ fun main() {
     val bam = BamFile.fromRaw(rawBam)
 
 
+    println(bam)
     var objs = mutableListOf<DrawTreeScope.DrawObject>()
     object : DrawTreeScope {
         override val bam: BamFile = bam
@@ -32,7 +37,121 @@ fun main() {
         bam.asModelRoot()!!
     )
 
-    println(Json.decodeFromStream<GlTF>(File("/tmp/untitled.gltf").inputStream()))
+    objs.forEachIndexed { index, it -> println("$index - ${it.texture}") }
+
+    val objIndex = 0
+    val obj = objs[objIndex]
+
+    println(obj)
+    val buffers = obj.vertexDatas.map {
+        val data = bam[it.data].data.toByteArray()
+        Buffer(byteLength = data.size.toLong(), uri = data.createDataUri())
+    }
+    val bufferViews = obj.vertexDatas.mapIndexed { index, it ->
+        val data = bam[it.data]
+        val format = bam[data.arrayFormat]
+        BufferView(
+            GlTFId(index.toLong()),
+            data.data.size.toLong(),
+            byteOffset = 0,
+            byteStride = format.stride.toLong(),
+            target = if (it.columns.contains(ArrayColumn.Index)) BufferViewTarget.ELEMENT_ARRAY_BUFFER else BufferViewTarget.ARRAY_BUFFER
+        )
+
+    }
+    val accessors = obj.vertexDatas.mapIndexed { index, vertexData ->
+        vertexData.columns.mapIndexedNotNull { columnIndex, it ->
+            val data = bam[vertexData.data]
+            val format = bam[data.arrayFormat]
+            val offset = vertexData.columns.take(columnIndex).fold(0L) { acc, x ->
+                acc + x.byteLen.toLong()
+            }
+            val (component, type) = when (it) {
+                ArrayColumn.Index -> ComponentType.UNSIGNED_SHORT to AccessorType.SCALAR
+                ArrayColumn.UV -> ComponentType.FLOAT to AccessorType.VEC2
+                ArrayColumn.Vertex -> ComponentType.FLOAT to AccessorType.VEC3
+                ArrayColumn.TransformBlend -> return@mapIndexedNotNull null
+            }
+            Accessor(
+                component,
+                (data.data.size / format.stride.toInt()).toLong(),
+                type,
+                byteOffset = offset,
+                bufferView = GlTFId(index.toLong()),
+                name = it.toString()
+            )
+        }
+    }.flatten()
+
+    val columns = obj.vertexDatas.flatMap { it.columns }.filter { it != ArrayColumn.TransformBlend }.withIndex()
+    val vertexId = columns
+        .firstNotNullOfOrNull { column -> column.index.takeIf { column.value == ArrayColumn.Vertex } }
+    val uvId = columns
+        .firstNotNullOfOrNull { column -> column.index.takeIf { column.value == ArrayColumn.UV } }
+    val indexId = columns
+        .firstNotNullOfOrNull { column -> column.index.takeIf { column.value == ArrayColumn.Index } }
+    val meshes = listOf(
+        when (obj.primitive) {
+            is GeomTrianglesImpl -> {
+                Mesh(
+                    listOf(
+                        MeshPrimitive(
+                            attributes = buildJsonObject {
+                                if (vertexId != null) put("POSITION", vertexId)
+                                if (uvId != null) put("TEXCOORD_0", uvId)
+                            },
+
+                            indices = if (indexId != null) GlTFId(indexId.toLong()) else null
+
+                        )
+                    )
+                )
+            }
+
+            is GeomTristripsImpl -> {
+                Mesh(
+                    listOf(
+                        MeshPrimitive(
+                            attributes = buildJsonObject {
+                                if (vertexId != null) put("POSITION", vertexId)
+                                if (uvId != null) put("TEXCOORD_0", uvId)
+                            },
+
+                            indices = if (indexId != null) GlTFId(indexId.toLong()) else null,
+                            mode = MeshPrimitiveMode.TRIANGLE_STRIP
+                        )
+                    )
+                )
+            }
+
+            else -> TODO()
+        }
+    )
+
+    val scenes = listOf(
+        Scene(nodes = listOf(GlTFId(0)))
+    )
+    val nodes = listOf(
+        Node(mesh = GlTFId(0), name = "Mesh${objIndex}")
+    )
+    val gltf = GlTF(
+        Asset("2.0", generator = "bam2gltf"),
+        scene = GlTFId(0),
+        buffers = buffers,
+        bufferViews = bufferViews,
+        accessors = accessors,
+        meshes = meshes,
+        scenes = scenes,
+        nodes = nodes
+    )
+
+    //println(json.encodeToString(gltf))
+    json.encodeToStream(gltf, File("/tmp/mine.gltf").outputStream())
+}
+
+fun ByteArray.createDataUri(): String {
+    val data = Base64.encode(this)
+    return "data:application/octet-stream;base64,$data"
 }
 
 interface DrawTreeScope {
